fd84783a0bd05faeb95974622a8d4dc215682179
MarcusLucinda <marcusvbl@hotmail.com>
2022-05-06 13:24:43 -0300

    Sistema de login

--- src/controller/route.js
+++ src/controller/route.js
@@ -9,7 +9,9 @@ router.get('/aluno/list', aluno_controller.list);
 router.get('/aluno/:id', aluno_controller.find);
 router.post('/turma/create', turma_controller.create);
 router.get('/turma/list', turma_controller.list);
-router.put('/turma/update/:id', turma_controller.update)
+router.put('/turma/entrar/:idTurma/:idGrupo', turma_controller.entrarGrupo)
 router.put('/turma/grupo/:id', turma_controller.createGrupo)
+router.delete('/turma/sair/:idTurma/:idGrupo', turma_controller.sairGrupo)
+router.post('/aluno/login', aluno_controller.login)
 
 module.exports = router;
